44d210f8184e77552fa9c11c4e1c4f337617e38e
soohyun-bae <qosuh58@gmail.com>
2024-12-13 14:27:26 +0900

    node.js2주차 과제

--- server.js
+++ server.js
@@ -1,61 +1,54 @@
 // server.js
 
-const http = require('http');
+const express = require('express');
+const cors = require('cors')
+const app = express()
+
+app.use(cors({
+  origin: "http://127.0.0.1:5500",
+  methods: ['OPTIONS', 'POST', 'GET', 'PUT', 'DELETE'],
+  headers: { 'Content-Type': 'application/json' }
+}))
+
+app.use(express.json())
+app.use(express.text())
+
 
 // CORS 설정을 위한 헤더
-const headers = {
-  'Access-Control-Allow-Origin': "http://127.0.0.1:9000",
-  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
+// const headers = {
+//   'Access-Control-Allow-Origin': "http://127.0.0.1:9000",
+//   'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
+//   'Access-Control-Allow-Headers': 'Content-Type',
+// };
 
 let data = { message: '여러분 화이팅!' };
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'OPTIONS') {
-    res.writeHead(204, headers);
-    res.end();
-    return;
-  }
+app.options('/', (req, res) => {
+  // res.writeHead(204, headers); -> res.writeHead(상태코드, 헤더정보) : 응답 헤더에 대한 정보를 기록하는 메서드
+  // cors에서 이 작업을 자동으러 처리해줌
+  return res.send()
+})
 
-  if (req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
-    res.end(JSON.stringify(data));
-  }
+app.get('/', (req, res) => {
+  // res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
+  return res.send(data);
+})
 
-  if (req.method === 'POST') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      data.message = body;
-      res.writeHead(200, headers);
-      res.end(`받은 POST 데이터: ${body}`);
-    });
-  }
-
-  if (req.method === 'PUT') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      data.message = body;
-      res.writeHead(200, headers);
-      res.end(`업데이트된 데이터: ${body}`);
-    });
-  }
-
-  if (req.method === 'DELETE') {
-    data = {};
-    res.writeHead(200, headers);
-    res.end('데이터가 삭제되었습니다.');
-  }
+app.post('/', (req, res) => {
+  data.message = req.body
+  res.send(`받은 POST 데이터: ${req.body}`);
 });
 
-server.listen(3000, () => {
+app.put('/',(req, res) => {
+  data.message = req.body
+  res.send(`받은 POST 데이터: ${req.body}`);
+})
+
+app.delete('/', (req, res) => {
+  data = {}
+  res.send('데이터가 삭제되었습니다.');
+})
+
+app.listen(3000, () => {
   console.log('서버가 http://localhost:3000/ 에서 실행 중입니다.');
 });
